758cb0f28f4c62c69f2741a36a983c0cfb0ef11e
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-11 16:12:45 +0200

    inserimento numeri da 1 a 100

---
+++ script.js
@@ -0,0 +1,9 @@
+const boxesContainer = document.querySelector(".boxes");
+
+for(let i = 0; i < 100; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.append(i + 1);
+    boxesContainer.append(box)
+    
+}
