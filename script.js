291e429dac56a787113f65ef564e693760cd944d
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-11 16:51:55 +0200

    modifiche

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const boxesContainer = document.querySelector(".boxes");
 
-for(let i = 0; i < 100; i++) {
+for(let i = 0; i <= 100; i++) {
     const box = document.createElement("div");
     box.classList.add("box");
     box.append(i + 1);
@@ -15,7 +15,7 @@ for(let i = 0; i < 100; i++) {
         box.classList.add("blu")
     }
 
-    else if(!((i + 1) % 3)){
+    else if(i % 3 == 0); {
         value = "fizz"
         box.classList.add("violet")
     }
