322ec7236446fe0d0a475870ff7990dd870fd9ce
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-11 17:29:21 +0200

    termine esercizio

--- script.js
+++ script.js
@@ -3,21 +3,27 @@ const boxesContainer = document.querySelector(".boxes");
 for(let i = 1; i <= 100; i++) {
     const box = document.createElement("div");
     box.classList.add("box");
-    box.append(i);
 
     if(i % 3 == 0 && i % 5 == 0 ) {
         value = "fizzbuzz"
         box.classList.add("acqua")
+        box.append("fizzbuzz")
     }
 
     else if(i % 5 == 0) {
         value = "buzz"
         box.classList.add("blu")
+        box.append("buzz")
     }
 
     else if(i % 3 == 0) {
         value = "fizz"
         box.classList.add("violet")
+        box.append("fizz")
+    }
+
+    else{
+        box.append(i);
     }
     
     boxesContainer.append(box);
