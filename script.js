49aab03f1babddb72d1731e7113830ca5bf9a43b
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-11 16:53:47 +0200

    modifiche

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ for(let i = 0; i <= 100; i++) {
     box.classList.add("box");
     box.append(i + 1);
 
-    if(!((i + 1) % 3 && (i + 1) % 5)){
+    if(i % 3 == 0); {
         value = "fizzbuzz"
         box.classList.add("acqua")
     }
 
-    else if(!((i + 1) % 5)){
+    else if(i % 5 == 0); {
         value = "buzz"
         box.classList.add("blu")
     }
